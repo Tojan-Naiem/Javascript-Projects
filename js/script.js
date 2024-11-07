a43ec4fd2f95cf694c59b69bae91c2747857ceb2
Tojann204 <tojan050@gmail.com>
2024-11-07 21:31:39 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-var inputEle=document.getElementById("input");
-var btn=document.getElementById("btn");
-var txt=document.getElementById("txt");
+// First one****************
+//var inputEle=document.getElementById("input");
+// var btn=document.getElementById("btn");
+// var txt=document.getElementById("txt");
 
 
-btn.onclick=function (){
-    var text=inputEle.value;
-    console.log(text);
-    txt.innerHTML=text;
-    inputEle.value="";
-}
+// btn.onclick=function (){
+//     var text=inputEle.value;
+//     console.log(text);
+//     txt.innerHTML=text;
+//     inputEle.value="";
+// }
+
+// The second one
+
+// var btn=document.getElementById("btn");
+// var div=document.getElementById("div");
+
+// var colors=['red','blue','green'];
+// var i=0;
+// btn.onclick=function(){
+//     div.style.backgroundColor=colors[i];
+//     i++;
+//     if(i==3)i=0;
+// }
